ff92c0f347b0e096faabac6ab2b1213c46bfcae9
You-Wen Ji <ohisyouwen.ji@gmail.com>
2020-04-07 22:31:20 +0200

    slide behavior & continuity from index

--- ScriptSyncExternalCodes_CSharp/Templates/Interlace.cs
+++ ScriptSyncExternalCodes_CSharp/Templates/Interlace.cs
@@ -53,14 +53,94 @@ public partial class Interlace : GH_ScriptInstance
     #endregion
 
 
-    private void RunScript(Mesh M, double S, ref object adM, ref object L, ref object N)
+    private void RunScript(Mesh M, double S, bool Reset, bool On, object Grab, ref object adM, ref object L, ref object N)
     {
         // <Custom code> 
-        List<Polyline> curves = new List<Polyline>();
-        List<int>[] nodeCurveID;
-        adM = ToInterlaceMesh(M, S, out curves, out nodeCurveID);
+        if(Reset || GoalList == null)
+        {
+            curves = new List<Polyline>();
+            adM = ToInterlaceMesh(M, S, out curves, out nodeCurveID);
+            
+            //kangaroo section
+            GoalList = new List<IGoal>();
+            PS = new PhysicalSystem();
+            List<int>[] kParticleIds = new List<int>[nodeCurveID.Length];
+            List<int> collisionIDs = new List<int>();
+            int id = -1;
+            for(int i = 0; i < nodeCurveID.Length; i++)
+            {
+                List<int> ids = new List<int>();
+                foreach (int j in nodeCurveID[i])
+                {
+                    //NOTE:
+                    //this adding standard sequence (0,1,2,3,4....) to kParticles not using nodeCurveID directly.
+                    id++;
+                    collisionIDs.Add(id);
+                    ids.Add(id);
+                }
+                kParticleIds[i] = ids;
+            }
+            //add particle and goal
+            for(int i = 0; i < curves.Count; i++)
+            {
+                for(int j = 0; j < curves[i].Count; j++)
+                {
+                    PS.AddParticle(curves[i][j], 0.0);
+                    if(j != 0)
+                    {
+                        IGoal spring = new KangarooSolver.Goals.Spring(kParticleIds[i][j - 1], kParticleIds[i][j], curves[i].SegmentAt(j - 1).Length, 30);
+                        GoalList.Add(spring);
+                    }
+                    if(j < curves[i].Count - 2)
+                    {
+                        IGoal angle = new KangarooSolver.Goals.Angle(30.0, 0.0, kParticleIds[i][j], kParticleIds[i][j + 1], kParticleIds[i][j + 1], kParticleIds[i][j + 2]);
+                        GoalList.Add(angle);
+                    }
+                    
+                    //if(curves[i].Count - 5 >= 0 || j <= curves[i].Count - 4) //012345
+                    //{
+                        //GoalObject continuity = new CurveContinuityID(new List<int> { kParticleIds[i][j], kParticleIds[i][j+1], kParticleIds[i][j+2], kParticleIds[i][j+3], kParticleIds[i][4] }, 10);
+                        //GoalList.Add(continuity);
+                    //}
+                    if(j == 0 || j == curves[i].Count -1)
+                    {
+                        IGoal anchor = new KangarooSolver.Goals.Anchor(kParticleIds[i][j], curves[i][j], 5);
+                        GoalList.Add(anchor);
+                    }
+                    //IGoal colinear = new KangarooSolver.Goals.CoLinear(kParticleIds[i], 0.01);
+                    //GoalList.Add(colinear);
+                }
+            }
+            for (int i = 0; i < nodeCurveID.Length; i++)
+            {
+                for (int j = 0; j < nodeCurveID[i].Count; j++)
+                {
+                    //Print(kParticleIds[i][j].ToString() + "," + nodeCurveID[i][j].ToString());
+                    IGoal slideOnCrv = new PointSlideOnCurve(kParticleIds[i][j], kParticleIds[nodeCurveID[i][j]], 0.2);
+                    GoalList.Add(slideOnCrv);
+                }
+            }
+            IGoal collision = new SphereCollideID(collisionIDs, 0.2, 1);
+            GoalList.Add(collision);
+            if (Grab != null)
+                GoalList.Add((IGoal)Grab);
+        }
+
+        if (On)
+        {
+            PS.Step(GoalList, true, 0.001);
+            int id = -1;
+            for(int i = 0; i < curves.Count; i++)
+            {
+                for(int j = 0; j < curves[i].Count; j++)
+                {
+                    id++;
+                    curves[i][j] = PS.GetPosition(id);
+                }
+            }
+        }
         L = curves;
-        
+        //N = PS.GetOutput(GoalList);
         #region Description
         Component.Params.Input[0].Description = "Mesh";
         Component.Params.Input[1].Description = "Amplitude";
@@ -69,6 +149,10 @@ public partial class Interlace : GH_ScriptInstance
     }
 
     // <Custom additional code> 
+    List<int>[] nodeCurveID;
+    List<Polyline> curves;
+    KangarooSolver.PhysicalSystem PS = new KangarooSolver.PhysicalSystem();
+    List<IGoal> GoalList = new List<IGoal>();
     public static Mesh ToInterlaceMesh(Mesh M, double Amplitude, out List<Polyline> WeaveCurves, out List<int>[] NodeBelongCurveID)
     {
         KPlanktonMesh kM = M.ToKPlanktonMesh();
@@ -126,7 +210,9 @@ public partial class Interlace : GH_ScriptInstance
 
         Curve[] jointCurves = Curve.JoinCurves(curves); //combine nodes to new curve, but any better solution?
         NodeBelongCurveID = new List<int>[jointCurves.Length];
-        for(int i = 0; i < jointCurves.Length; i ++)
+        //NOTE: NodeBelongCurveID
+        //[curve index]: node interlace to another curve index.......
+        for (int i = 0; i < jointCurves.Length; i ++)
         {
             Polyline pCurve;
             jointCurves[i].TryGetPolyline(out pCurve);
@@ -155,9 +241,181 @@ public partial class Interlace : GH_ScriptInstance
             }
             NodeBelongCurveID[i] = ids;
         }
-
-
         return wM;
+    }
+    public class PointSlideOnCurve : GoalObject
+    {
+        public double strength;
+        public PointSlideOnCurve(int NodeID, List<int> TargetCurveID, double k)
+        {
+            int L = TargetCurveID.Count + 1;
+            int[] Points = new int[L];
+            Points[0] = NodeID;
+            for(int i = 0; i < TargetCurveID.Count; i++)
+                Points[i + 1] = TargetCurveID[i];
+            PIndex = Points;
+            Move = new Vector3d[L];
+            Weighting = new double[L];
+            for (int j = 0; j < L; j++)
+                Weighting[j] = k;
+            strength = k;
+        }
+        public override void Calculate(List<KangarooSolver.Particle> p)
+        {
+            int L = PIndex.Length;
+            for(int i = 0; i < L; i++)
+            {
+                Move[i] = Vector3d.Zero;
+                Weighting[i] = strength;
+            }
+
+            int[] curveIDs = new int[L - 1];
+            for (int i = 1; i < L; i++)
+                curveIDs[i - 1] = PIndex[i];
+            List<Point3d> pts = new List<Point3d>();
+            foreach(int i in curveIDs)
+            {
+                pts.Add(p[i].Position);
+            }
+            PolylineCurve polyline = new PolylineCurve(pts);
+            double t;
+            polyline.ClosestPoint(p[PIndex[0]].Position, out t);
+            Vector3d toClosetPt = polyline.PointAt(t) - p[PIndex[0]].Position;
+            toClosetPt.Unitize();
+            Move[0] = toClosetPt * Weighting[0];
+        }
+        public override object Output(List<KangarooSolver.Particle> p)
+        {
+            int L = PIndex.Length;
+            List<Point3d> pts = new List<Point3d>();
+            for (int i = 1; i < L; i++)
+                pts.Add(p[PIndex[i]].Position);
+            Polyline polyline = new Polyline(pts);
+            return polyline;
+        }
+    }
+    public class SphereCollideID : GoalObject
+    {
+        public double Strength;
+        public double SqDiam;
+        public double Diam;
+
+        public SphereCollideID(List<int> V, double r, double k)
+        {
+            int L = V.Count;
+            PIndex = V.ToArray();
+            Move = new Vector3d[L];
+            Weighting = new double[L];
+            for (int i = 0; i < L; i++)
+            {
+                Weighting[i] = k;
+            }
+            Diam = r + r;
+            SqDiam = Diam * Diam;
+            Strength = k;
+        }
+
+        public override void Calculate(List<KangarooSolver.Particle> p)
+        {
+            int L = PIndex.Length;
+            double[] Xcoord = new double[L];
+            for (int i = 0; i < L; i++)
+            {
+                Xcoord[i] = p[PIndex[i]].Position.X;
+            }
+            Array.Sort(Xcoord, PIndex);
+
+            for (int i = 0; i < L; i++)
+            {
+                Move[i] = Vector3d.Zero;
+                Weighting[i] = 0;
+            }
+
+            for (int i = 0; i < (PIndex.Length - 1); i++)
+            {
+                for (int j = 1; (i + j) < PIndex.Length; j++)
+                {
+                    int k = i + j;
+                    Vector3d Separation = p[PIndex[k]].Position - p[PIndex[i]].Position;
+                    if (Separation.X < Diam)
+                    {
+                        if (Separation.SquareLength < SqDiam)
+                        {
+                            double LengthNow = Separation.Length;
+                            double stretchfactor = 1.0 - Diam / LengthNow;
+                            Vector3d SpringMove = 0.5 * Separation * stretchfactor;
+                            Move[i] += SpringMove;
+                            Move[k] -= SpringMove;
+                            Weighting[i] = Strength;
+                            Weighting[k] = Strength;
+                        }
+                    }
+
+                    else { break; }
+                }
+            }
+        }
+    }
+    //NOTE 0407: 
+    //still need to debug continuity function in P2 & Move[2] will cause error (null point) in solver.
+    //remain not implement and question necessity in bend behavior.
+    public class CurveContinuityID: GoalObject
+    {
+        public CurveContinuityID(List<int> ids, double k)
+        {
+            PIndex = ids.ToArray();
+            Move = new Vector3d[5];
+            Weighting = new double[] { k, k, k, k, k };
+        }
+
+        public override void Calculate(List<KangarooSolver.Particle> p)
+        {
+            Point3d P0 = p[PIndex[0]].Position;
+            Point3d P1 = p[PIndex[1]].Position;
+            Point3d P2 = p[PIndex[2]].Position;
+            Point3d P3 = p[PIndex[3]].Position;
+            Point3d P4 = p[PIndex[4]].Position;
+            Vector3d V31 = P3 - P1;
+            Vector3d V21 = P2 - P1;
+            double length = V31.Length;
+            V31.Unitize();
+            double num = V21 * V31;
+            Move[2] = -0.25 * (V21 - num * V31);
+            double num2 = num / length;
+            Move[1] = (1.0 - num2) * - Move[2];
+            Move[3] = num2 * - Move[2];
+            double length2 = V21.Length;
+            double num3 = P2.DistanceTo(P3);
+            Vector3d V10 = P1 - P0;
+            num = V10 * V31;
+            Vector3d val4 = V10 - V31 * num;
+            double length3 = val4.Length;
+            Vector3d val5 = P3 - P4;
+            num = val5 * V31;
+            Vector3d val6 = val5 - V31 * num;
+            double length4 = val6.Length;
+            double num4 = length2 * length2 / (2.0 * num3 * num3) + length3 / (2.0 * length4);
+            double num5 = Math.Sqrt(num4 * num3 * num3);
+            double num6 = Math.Sqrt(length2 * length2 / num4);
+            double num7 = num4 * length4;
+            double num8 = length3 / num4;
+            double num9 = length2 - num5;
+            double num10 = num3 - num6;
+            double num11 = length3 - num7;
+            double num12 = length4 - num8;
+            V10.Unitize();
+            Move[0] = 0.1 * V10 * num11;
+            Move[1] -= 0.5 * Move[0];
+            val5.Unitize();
+            Move[4] = 0.1 * val5 * num12;
+            Move[3] -= 0.5 * Move[4];
+            Vector3d val7 = 0.1 * V31 * num9;
+            Move[1] += val7;
+            Move[2] -= val7;
+            val7 = 0.1 * V31 * num10;
+            Move[1] -= val7;
+            Move[2] += val7;
+        }
     }
     // </Custom additional code>  
 }
